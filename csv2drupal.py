4a3482f91db8ffddc011cb7adaf3ff53988126ff
Nathan Gilbert <546222+nathan-gilbert@users.noreply.github.com>
2018-06-03 16:27:09 -0600

    working with scorecard

--- csv2drupal.py
+++ csv2drupal.py
@@ -1,9 +1,33 @@
 
 import sys
 import csv
+import string
+
+tr = string.Template("""
+<tr>
+  <td style="text-align: left;">HD-${District}</td>
+  <td style="text-align: left;">${Representative}</td>
+  <td style="text-align: right;">${PercentGrade}</td>
+  <td style="text-align: center;">${LetterGrade}</td>
+</tr>
+""")
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print(row)
+            #print(str(row) + "\n")
+            try:
+                rep_name_tokens = row['Representative'].split(", ")
+                rep_name = rep_name_tokens[1].strip() + " " + rep_name_tokens[0]
+            except:
+                rep_name = row['Representative']
+
+            v = {
+                'District': row['District '],
+                #'Representative' : row['Representative'],
+                'Representative' : rep_name,
+                'PercentGrade' : row['Percent Grade'],
+                'LetterGrade': row['Letter Grade']
+            }
+            print(tr.substitute(v))
